9f4c5ba8c4d7826542f800a4cdde23eb2e1f40ea
PCNAME <PCNAME@DESKTOP-IB6GJ2Q>
2022-06-20 15:51:06 +0530

    Added sysout message

--- B6DevOpsTelus/src/com/telus/sample/WelcomeUser.java
+++ B6DevOpsTelus/src/com/telus/sample/WelcomeUser.java
@@ -5,6 +5,9 @@ public class WelcomeUser {
 	public static void main(String[] args) {
 		// Code added by Vinoth
 		System.out.println("Welcome to DevOps Class");
+		
+		// Code added by Pranav
+		System.out.println("Thank you for the access");
 
 	}
 
