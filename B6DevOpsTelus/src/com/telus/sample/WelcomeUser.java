e613ab72d05bb0dd1945909b99520c2589ce4e7a
Vinoth Rathinam <vinothrwins@gmail.com>
2022-06-20 15:40:23 +0530

    Adding new project

---
+++ B6DevOpsTelus/src/com/telus/sample/WelcomeUser.java
@@ -0,0 +1,11 @@
+package com.telus.sample;
+
+public class WelcomeUser {
+
+	public static void main(String[] args) {
+		// Code added by Vinoth
+		System.out.println("Welcome to DevOps Class");
+
+	}
+
+}
